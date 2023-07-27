e5c089e23311a4bac029a0cf2d6e739353ee73a1
salem676 <echoesofeternal@gmail.com>
2023-07-27 14:39:48 -0600

    jessy

--- blockchain/v0.2/block_mine.c
+++ blockchain/v0.2/block_mine.c
@@ -1,19 +1,15 @@
 #include "blockchain.h"
 
 /**
- * block_mine - mines a block according to the difficulty
- * @block: pointer to block to mine
+ * block_mine - mines a block
+ * @block: pointer to the block to be mined
+ * Return: nothing (void)
  */
 void block_mine(block_t *block)
 {
-	uint64_t nonce = 0;
-
-	if (!block)
-		return;
-
+	block->info.nonce = 0;
 	do {
-		block->info.nonce = nonce;
+		block->info.nonce += 1;
 		block_hash(block, block->hash);
-		nonce++;
 	} while (!hash_matches_difficulty(block->hash, block->info.difficulty));
 }
